c4c453b1a77b195fc4f48b8eaf8e5a771eedc670
Joris Boeye <joris.boeye@gmail.com>
2021-12-01 23:54:25 +0100

    Improve solution.

--- day01/part2.py
+++ day01/part2.py
@@ -1,21 +1,15 @@
 from pathlib import Path
 
 
-def generate_window(numbers):
-    for i, j, k in zip(numbers, numbers[1:], numbers[2:]):
-        yield sum([i, j, k])
-
-
-def generate_increasing(numbers):
-    for i, j in zip(numbers, numbers[1:]):
+def generate_increasing(numbers, step=1):
+    for i, j in zip(numbers, numbers[step:]):
         yield j > i
 
 
 def solve(file: str = "input.txt"):
     with open(Path(__file__).parent / file, "r") as f:
         numbers = [int(line) for line in f.read().splitlines()]
-    windows = list(generate_window(numbers=numbers))
-    return sum(generate_increasing(numbers=windows))
+    return sum(generate_increasing(numbers=numbers, step=3))
 
 
 if __name__ == "__main__":
