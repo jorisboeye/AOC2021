a611da9c123f77ba4f4e06fe3f2ca2ad3369e0db
Joris Boeye <joris.boeye@gmail.com>
2021-12-14 22:05:03 +0100

    Refactor.

--- day01/part2.py
+++ day01/part2.py
@@ -1,16 +1,44 @@
 from pathlib import Path
+from typing import Union
+
+import pytest
+
+from aoc.parsers import parse_lines, read
+from aoc.submit import submit
+
+FILE = Path(__file__)
+TEST_RESULT = 5
+TEST_INPUT = """\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
 
 
-def generate_increasing(numbers, step=1):
-    for i, j in zip(numbers, numbers[step:]):
-        yield j > i
+def solve(puzzle_input: Union[str, Path]) -> int:
+    numbers = tuple(parse_lines(read(puzzle_input), target=int))
+    return sum(j > i for i, j in zip(numbers, numbers[3:]))
 
 
-def solve(file: str = "input.txt"):
-    with open(Path(__file__).parent / file, "r") as f:
-        numbers = [int(line) for line in f.read().splitlines()]
-    return sum(generate_increasing(numbers=numbers, step=3))
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
 
 
 if __name__ == "__main__":
-    print(solve())
+    test_answer = solve(TEST_INPUT)
+    if test_answer == TEST_RESULT:
+        answer = solve(FILE.parent / "input.txt")
+        submit(answer=answer, file=FILE)
+    else:
+        print(test_answer)
