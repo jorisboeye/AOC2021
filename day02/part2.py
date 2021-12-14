e1521ae30bf9a72c0016eff9ee5348d65ae9a482
Joris Boeye <joris.boeye@gmail.com>
2021-12-14 22:24:09 +0100

    Refactor

--- day02/part2.py
+++ day02/part2.py
@@ -1,18 +1,31 @@
 from pathlib import Path
-from re import X
-from typing import NamedTuple
+from typing import Union
 
 import attr
-from aocd import submit
+import pytest
+
+from aoc.parsers import parse_lines, read, split_mapping
+from aoc.submit import submit
+
+FILE = Path(__file__)
+TEST_RESULT = 900
+TEST_INPUT = """\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
 
 
-@attr.define
+@attr.s(auto_attribs=True)
 class Position:
     horizontal: int
     depth: int
     aim: int
 
-    def move(self, direction, x):
+    def move(self, direction: str, x: str) -> None:
         if direction == "forward":
             self.horizontal += int(x)
             self.depth += int(x) * self.aim
@@ -22,23 +35,30 @@ class Position:
             self.aim -= int(x)
 
     @property
-    def result(self):
+    def result(self) -> int:
         return self.horizontal * self.depth
 
 
-def parse_input(file):
-    return [line.split() for line in file.read().splitlines()]
-
-
-def solve(file: str = "input.txt"):
-    with open(Path(__file__).parent / file, "r") as f:
-        instructions = parse_input(f)
+def solve(puzzle_input: Union[str, Path]) -> int:
+    instructions = parse_lines(read(puzzle_input), split_mapping(" "))
     position = Position(0, 0, 0)
     for instruction in instructions:
         position.move(*instruction)
     return position.result
 
 
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
+
+
 if __name__ == "__main__":
-    print(solve())
-    submit(solve(), part="b", day=2)
+    test_answer = solve(TEST_INPUT)
+    if test_answer == TEST_RESULT:
+        answer = solve(FILE.parent / "input.txt")
+        submit(answer=answer, file=FILE)
+    else:
+        print(test_answer)
