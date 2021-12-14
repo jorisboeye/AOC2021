624dcbf3917b889bd4a25bee79c640a4accdcb60
Joris Boeye <joris.boeye@gmail.com>
2021-12-14 22:25:43 +0100

    Add test.

--- day00/part1.py
+++ day00/part1.py
@@ -1,16 +1,29 @@
 from pathlib import Path
 from typing import Union
 
+import pytest
+
 from aoc.parsers import parse_lines, read
 from aoc.submit import submit
 
 FILE = Path(__file__)
 TEST_RESULT = 0
+TEST_INPUT = """\
+
+"""
 
 
 def solve(puzzle_input: Union[str, Path]) -> int:
     numbers = tuple(parse_lines(text=read(puzzle_input), target=int))
     return len(numbers)
+
+
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
 
 
 if __name__ == "__main__":
