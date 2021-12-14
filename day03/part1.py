a41586a1fbef4484d54e3dd14b1b1c23c9f35b84
Joris Boeye <joris.boeye@gmail.com>
2021-12-14 22:47:03 +0100

    Refactor day 3.

--- day03/part1.py
+++ day03/part1.py
@@ -1,15 +1,31 @@
 from pathlib import Path
+from typing import Union
 
-from aocd import submit
+import pytest
+
+from aoc.parsers import parse_lines, read
+from aoc.submit import submit
+
+FILE = Path(__file__)
+TEST_RESULT = 198
+TEST_INPUT = """\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+"""
 
 
-def parse_input(file):
-    return [list(line) for line in file.read().splitlines()]
-
-
-def solve(file: str = "input.txt"):
-    with open(Path(__file__).parent / file, "r") as f:
-        numbers = parse_input(f)
+def solve(puzzle_input: Union[str, Path]) -> int:
+    numbers = tuple(parse_lines(read(puzzle_input), target=list))
     vals = [sum([int(n) for n in bits]) for bits in zip(*numbers)]
     result = [int((val) / len(numbers) > 0.5) for val in vals]
     gamma = sum([v * 2 ** p for p, v in enumerate(reversed(result))])
@@ -18,7 +34,18 @@ def solve(file: str = "input.txt"):
     return gamma * epsilon
 
 
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
+
+
 if __name__ == "__main__":
-    print(solve("test_input.txt"))
-    if solve("test_input.txt") == 198:
-        submit(solve(), part="a", day=3)
+    test_answer = solve(TEST_INPUT)
+    if test_answer == TEST_RESULT:
+        answer = solve(FILE.parent / "input.txt")
+        submit(answer=answer, file=FILE)
+    else:
+        print(test_answer)
--- day03/part2.py
+++ day03/part2.py
@@ -1,44 +1,77 @@
 from pathlib import Path
+from typing import Callable, Iterable, List, Tuple, Union
 
-from aocd import submit
+import pytest
+
+from aoc.parsers import parse_lines, read
+from aoc.submit import submit
+
+FILE = Path(__file__)
+TEST_RESULT = 230
+TEST_INPUT = """\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+"""
+
+Numbers = Tuple[List[int], ...]
 
 
-def parse_input(file):
-    return [[int(n) for n in line] for line in file.read().splitlines()]
-
-
-def oxygen_rating(numbers):
+def oxygen_rating(numbers: Numbers) -> Iterable[int]:
     for index_values in zip(*numbers):
         yield int(sum(index_values) / len(numbers) >= 0.5)
 
 
-def co2_rating(numbers):
+def co2_rating(numbers: Numbers) -> Iterable[int]:
     for index_values in zip(*numbers):
         yield int(sum(index_values) / len(numbers) < 0.5)
 
 
-def filter_numbers(numbers, rating_function):
+def filter_numbers(
+    numbers: Numbers, rating_function: Callable[[Numbers], Iterable[int]]
+) -> List[int]:
     position = 0
     while len(numbers) > 1:
-        rating = list(rating_function(numbers))
-        numbers = [n for n in numbers if n[position] == rating[position]]
+        rating = tuple(rating_function(numbers))
+        numbers = tuple(n for n in numbers if n[position] == rating[position])
         position += 1
     return numbers[0]
 
 
-def decimal(binary):
+def decimal(binary: List[int]) -> int:
     return sum([v * 2 ** p for p, v in enumerate(reversed(binary))])
 
 
-def solve(file: str = "input.txt"):
-    with open(Path(__file__).parent / file, "r") as f:
-        numbers = parse_input(f)
+def solve(puzzle_input: Union[str, Path]) -> int:
+    numbers = tuple(
+        parse_lines(read(puzzle_input), target=lambda x: [int(n) for n in x])
+    )
     return decimal(filter_numbers(numbers, oxygen_rating)) * decimal(
         filter_numbers(numbers, co2_rating)
     )
 
 
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    ((TEST_INPUT, TEST_RESULT),),
+)
+def test(test_input: str, expected: int) -> None:
+    assert solve(test_input) == expected
+
+
 if __name__ == "__main__":
-    print(solve("test_input.txt"))
-    if solve("test_input.txt") == 230:
-        submit(solve(), part="b", day=3)
+    test_answer = solve(TEST_INPUT)
+    if test_answer == TEST_RESULT:
+        answer = solve(FILE.parent / "input.txt")
+        submit(answer=answer, file=FILE)
+    else:
+        print(test_answer)
